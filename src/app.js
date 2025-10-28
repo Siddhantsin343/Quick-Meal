8e90f8a0eee07617c501196afc800c630c88aa53
siddhant <siddhantsin343@gmail.com>
2025-10-28 22:55:02 +0530

    add Header Comp

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 
 // Css
 import './index.css'
+import Header from "./compontents/Header";
+
+
+/*---- Compontents ---- */
+
+
+
+const App = ()=>(
+<Header/>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App/>)
