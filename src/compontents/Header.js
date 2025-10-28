f5fd4ac84b75675b9eb09b351143c2feee6335c0
siddhant <siddhantsin343@gmail.com>
2025-10-28 22:30:19 +0530

    new Header Compontend added!

---
+++ src/compontents/Header.js
@@ -0,0 +1,21 @@
+/* Header Compontent*/
+
+const Header = ()=>{
+    return(
+        <div className="navCon">
+            <div className="imgCon">
+                <img src="" alt="logo-Image"/>
+            </div>
+            <div className="navItem">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
