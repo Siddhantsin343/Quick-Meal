e36fe15ad997877b6e3fe6138a5ae9be7cccc15c
siddhant <siddhantsin343@gmail.com>
2025-10-29 20:43:44 +0530

    some update

---
+++ src/compontents/Body.js
@@ -0,0 +1,18 @@
+const Body =  () =>(
+    <div className="bodyCon">
+        <div className="searchBox">
+            <input type="text" placeholder="search-Here"></input>
+            <button>search</button>
+        </div>
+        <div className="filterBox">
+            <button className="filterBtn">
+                filter Restaurent
+            </button>
+        </div>
+        <div className="CardCon">
+            
+        </div>
+    </div>
+)
+
+export default Body;
