763ccf048bacbe41bfcd51bbc51269ce85ac41ac
siddhant <siddhantsin343@gmail.com>
2025-10-30 22:20:15 +0530

    - LOGO URL change

--- src/compontents/Header.js
+++ src/compontents/Header.js
@@ -1,10 +1,11 @@
 /* Header Compontent*/
+import { LOGO_URL } from "../utils/constant";
 
 const Header = ()=>{
     return(
         <div className="navCon">
             <div className="imgCon">
-                <img src="https://tse4.mm.bing.net/th/id/OIP.aEFj52jeL9cEwAK3oZYmhgHaHa?pid=Api&P=0&h=180" alt="logo-Image"/>
+                <img src={LOGO_URL} alt="logo-Image"/>
             </div>
             <div className="navItem">
                 <ul>
