5115c80b6e1abb918d8c211183df421054884cd3
siddhant <siddhantsin343@gmail.com>
2025-10-30 15:14:52 +0530

    Card compontent

---
+++ src/compontents/Card.js
@@ -0,0 +1,15 @@
+const Card = ()=>{
+    return(
+        <div className="card">
+            <div className="cardImg">
+                <img src="" alt="Card Image"/>
+            </div>
+            <h1>Restaurent Name</h1>
+            <h2>dishes</h2>
+            <h3>delivery Time.</h3>
+            <h3>star</h3>
+        </div>
+    )
+}
+
+export default Card;
