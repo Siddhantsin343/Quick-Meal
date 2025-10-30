6c2774bbf544056942f4c182f298521d0ce1627e
siddhant <siddhantsin343@gmail.com>
2025-10-30 23:07:43 +0530

    - shimmer ui Compontent

---
+++ src/compontents/Shimmer.js
@@ -0,0 +1,12 @@
+// ShimmerCard.js
+const ShimmerCard = () => {
+  return (
+    <div className="shimmer-card">
+      <div className="shimmer-img shimmer"></div>
+      <div className="shimmer-text shimmer"></div>
+      <div className="shimmer-subtext shimmer"></div>
+    </div>
+  );
+};
+
+export default ShimmerCard;
