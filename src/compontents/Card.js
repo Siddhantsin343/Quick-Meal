f2f21ac0a0212e3f29e2f136d08228172e440510
Siddhant Singh <135977313+Siddhantsin343@users.noreply.github.com>
2025-10-31 20:09:08 +0530

    Update Card component to use dynamic image and props

--- src/compontents/Card.js
+++ src/compontents/Card.js
@@ -1,13 +1,17 @@
-const Card = ()=>{
+import { IMG } from "../utils/constant";
+
+const Card = (props)=>{
+    const {reLis} = props
+    const {name, cloudinaryImageId, cuisines, avgRating, sla} = reLis
     return(
         <div className="card">
             <div className="cardImg">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/24/15345730-3e92-4301-8bf8-d3d327d96ce2_1013619.jpg" alt="Card Image"/>
+                <img src={IMG + cloudinaryImageId} alt="Card Image"/>
             </div>
-            <h1>Restaurent Name</h1>
-            <h2>dishes</h2>
-            <h3>delivery Time.</h3>
-            <h3>star</h3>
+            <h1>{name}</h1>
+            <h2>{cuisines.join(" ,")}</h2>
+            <h3>rating ⭐ {avgRating}</h3>
+            <h3>deliver in {sla?.deliveryTime} min</h3>
         </div>
     )
 }
